309acb3bdc2480f169c9c0525b7e1f36fa588d8a
Jorge Lopes <jorgedclopes@gmail.com>
2023-05-15 22:58:10 +0100

    Remove tokens. Elaborate README, now for real.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # eldrida
+
+This is eldrida, an integration bot that reads discord messages, queries chatGPT and provides a response back to discord.
+
+## How to start bot
+
+1. Get OpenAI token.
+2. Get discord bot token.
+3. Add them to ./start.sh just as plain text. Do not add quotation mark or anything.
+4. Run `./start.sh` (might need to change permissions with `chmod +x ./start.sh`).
--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DISCORD_KEY=
+OPENAI_API_KEY=
+
 docker container stop delaney || true
 docker container rm delaney || true
 
@@ -8,7 +11,7 @@ VERSION=$1
 docker build . --file Dockerfile --tag carequinha/eldrida:"$VERSION"
 docker run -d \
 --name eldrida \
--e DISCORD_KEY=MTEwNzA0ODM5NjMzNTg5MDQ0Mw.GiTn7S.c3WWPx5sZJsJ0OWMh3efwvyDcXv6s44dBaqbt8 \
--e OPENAI_API_KEY=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 \
+-e DISCORD_KEY=$DISCORD_KEY \
+-e OPENAI_API_KEY=$OPENAI_API_KEY \
 --restart always \
 carequinha/eldrida:"$VERSION"
